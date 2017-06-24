427a1862afc785188cfb478efe3fd61e564457e2
diewland <diewland@gmail.com>
2017-06-24 11:49:19 +0700

    all logins worked

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+// This config file is for the demo only. It is in .gitignore and will not be pushed to git repo.
+var config = {
+    apiKey: "AIzaSyBi2eKbCreXTrxpAY5MrBDMi6f-arySOhQ",
+    authDomain: "hellostranger-a3014.firebaseapp.com",
+    databaseURL: "https://hellostranger-a3014.firebaseio.com",
+    projectId: "hellostranger-a3014",
+    storageBucket: "hellostranger-a3014.appspot.com",
+    messagingSenderId: "549083960147"
+};
+firebase.initializeApp(config);
