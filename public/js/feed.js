bdbc4126620413ca320fdc4558f88de8c11034f9
diewland <diewland@gmail.com>
2017-06-25 09:42:52 +0700

    Handled allow-friends checklist

--- public/js/feed.js
+++ public/js/feed.js
@@ -3,6 +3,7 @@ $('.new.red.btn-floating').click(function(){
   $('#post-modal').modal('open');
 
   if($('.allow-friends .progress').length == 1){
+    // query friend list at firsttime
     get_all_friends().then(function(friends){
       $('#post-modal .allow-friends').html('');
       if(friends){
@@ -17,4 +18,19 @@ $('.new.red.btn-floating').click(function(){
       }
     });
   }
+  else {
+    // checked all friends by default
+    $('.allow-friends input[type=checkbox]').prop('checked', true);
+  }
+});
+
+// submit post item
+$('#post-modal #submit').click(function(){
+  var uids = $.map($('.allow-friends input:checked'), function(r, i){
+    return r.value;
+  });
+
+  // TODO collect modal data -> post new
+  alert(uids);
+
 });
